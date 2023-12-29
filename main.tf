9d2f3f875e7d0fbf4d557101a903f0e64171d845
RJGcp <ravichanderkt@gmail.com>
2023-12-29 11:15:06 +0000

    Subnet details has been updated in the main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ resource "google_container_cluster" "gke_cluster" {
   location = var.gcp_region
 
   network    = google_compute_network.private_network.name
-  subnetwork = data.google_compute_subnetwork.subnet.name
+  subnetwork = data.google_compute_subnetwork.subnet.self_link
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
 
   remove_default_node_pool = true
   initial_node_count       = 1
