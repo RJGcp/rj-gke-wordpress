812863b900f2d79009d1fc3de440ee3be976c061
RJGcp <ravichanderkt@gmail.com>
2024-01-07 16:14:43 +0000

    Removed unnecessary outputs.

--- output.tf
+++ output.tf
@@ -10,18 +10,6 @@ output "gke_cluster_ca_certificate" {
   value = google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
 }
 
-output "mysql_instance_connection_name" {
-  value = google_sql_database_instance.mysql_instance.connection_name
-}
-
-output "mysql_database_name" {
-  value = google_sql_database.wordpress_database.name
-}
-
-output "client_cert" {
-  value = google_sql_ssl_cert.sql_ssl_cert.client_cert
-}
-
 output "client_key" {
   value = google_sql_ssl_cert.sql_ssl_cert.client_key
 }
