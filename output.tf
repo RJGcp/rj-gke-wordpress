3f8c53b80c257c841c9a1a6d872f35e62bde814a
RJGcp <ravichanderkt@gmail.com>
2024-01-07 16:22:05 +0000

    Output TF conf has been updated.

--- output.tf
+++ output.tf
@@ -10,10 +10,25 @@ output "gke_cluster_ca_certificate" {
   value = google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
 }
 
-output "client_key" {
-  value = google_sql_ssl_cert.sql_ssl_cert.client_key
+
+############## Cloud SQL ################ ++++ ############## Outputs ######################
+
+output "sql_instance_name" {
+  description = "The name of the Google Cloud SQL instance."
+  value       = google_sql_database_instance.sql_instance.name
 }
 
-output "server_ca" {
-  value = google_sql_ssl_cert.sql_ssl_cert.server_ca
+output "sql_instance_connection_name" {
+  description = "The connection name of the Google Cloud SQL instance to be used in connection strings."
+  value       = google_sql_database_instance.sql_instance.connection_name
+}
+
+output "sql_instance_region" {
+  description = "The region of the Google Cloud SQL instance."
+  value       = google_sql_database_instance.sql_instance.region
+}
+
+output "sql_instance_ip_address" {
+  description = "The assigned IP address of the Google Cloud SQL instance."
+  value       = google_sql_database_instance.sql_instance.ip_address
 }
